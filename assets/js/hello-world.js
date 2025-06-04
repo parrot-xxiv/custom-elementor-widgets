ccfd5a909592bdf9fa9a16b8263dd6c2d3433fa3
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-06-04 23:37:21 +0800

    add reference

--- assets/js/hello-world.js
+++ assets/js/hello-world.js
@@ -1,1 +1,9 @@
-console.log('hello world widget is being used in this page')
+(function ($) {
+    'use strict';
+    $(document).ready(function () {
+        function helloWorldWidget($scope, $) {
+            console.log($scope,$)
+        }
+        elementorFrontend.hooks.addAction('frontend/element_ready/hello_world.default', helloWorldWidget);
+    });
+})(jQuery);
